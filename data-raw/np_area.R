9d6df2bacbee9e7bf0ccbe1c2f5e7682528fd189
Shinya Uryu <uryu.shinya@nies.go.jp>
2019-01-11 11:19:37 +0900

    Update raw data
    奄美群島が追加された、shapefileの情報が正確になった

--- data-raw/np_area.R
+++ data-raw/np_area.R
@@ -8,48 +8,39 @@ library(dplyr)
 library(testthat)
 library(ggplot2) # 2.2.1.9000
 
-sf_np <- st_read("data-raw/nps/nps_1.shp") %>%
+sf_np <-
+  st_read(here::here("data-raw/nps_all/nps_all.shp"), as_tibble = TRUE) %>%
+  st_transform(crs = 4326) %>%
   rlang::set_names(c("name", "area", "geometry"))
-expect_equal(dim(sf_np), c(8849, 3))
-
-sf_np$name %>% n_distinct() # 34 より多い
-sf_np$name %>% unique()
+expect_equal(dim(sf_np), c(8789, 3))
+expect_equal(sf_np$name %>% n_distinct(), 35L)
 
 # Modified ----------------------------------------------------------------
 # nameから34公園で区分できるように「編集」
 # 編集なのでデータの出典明記を変更する必要あり
-df_np <- readr::read_csv("data-raw/np_list.csv")
+# source(here::here("data-raw/np_information.R"))
+df_np <-
+  readr::read_csv(here::here("data-raw/np_list.csv"))
 
-sf_np_modified <- sf_np %>%
-  mutate(name = stringr::str_replace(name, "\\（.+?\\）", "")) %>%
-  mutate(name = recode(name,
-                       `屋久島・乗り入れ規制` = "屋久島",
-                       `屋久島・乗入れ規制地` = "屋久島",
-                       `阿蘇くじゅう（阿蘇地` = "阿蘇くじゅう",
-                       `やんばる国立公園` = "やんばる",
-                       `石西礁湖・西表島・鳩間島周辺` = "西表石垣",
-                       `石西礁湖` = "西表石垣",
-                       `鳩間島・西表島` = "西表石垣",
-                       `石垣地域` = "西表石垣",
-                       `西表地域` = "西表石垣",
-                       `嘉弥間島` = "西表石垣",
-                       `新城島` = "西表石垣",
-                       `鳩間島` = "西表石垣",
-                       `仲御神島` = "西表石垣",
-                       `竹富島` = "西表石垣",
-                       `小浜島` = "西表石垣",
-                       `黒島` = "西表石垣",
-                       `波照間島` = "西表石垣",
-                       `利尻例文サロベツ` = "利尻礼文サロベツ",
-                       `阿寒` = "阿寒摩周"
-                       ))
-
-expect_equal(
-  unique(sf_np_modified$name)[!unique(sf_np_modified$name) %in% df_np$name],
-  character(0)
+expect_length(
+  unique(sf_np$name)[!unique(sf_np$name) %in% df_np$name],
+  2L
 )
 
-sf_np_union <- sf_np_modified %>%
+sf_np_modified <-
+  sf_np %>%
+  mutate(name = recode(name,
+                       `奄美大島` = "奄美群島",
+                       `利尻例文サロベツ` = "利尻礼文サロベツ"
+                       ))
+
+expect_length(
+  unique(sf_np_modified$name)[!unique(sf_np_modified$name) %in% df_np$name],
+  0
+)
+# 公園ごとに一つのポリゴン (保護区でわけない)
+sf_np_union <-
+  sf_np_modified %>%
   split(.$name) %>%
   purrr::map(~ st_buffer(.x, dist = 0.0001) %>%
                st_union() %>%
@@ -57,7 +48,8 @@ sf_np_union <- sf_np_modified %>%
   purrr::reduce(c) %>%
   as_tibble() %>%
   st_sf()
-sf_np_union$name <- sort(unique(sf_np_modified$name))
+sf_np_union$name <-
+  sort(unique(sf_np_modified$name))
 
 # mapの過程でやるとサイズが合わなくなる...
 names(sf_np_union) <- c("geometry", "name")
@@ -66,7 +58,9 @@ names(sf_np_union) <- c("geometry", "name")
 # leaflet() %>% addTiles() %>% addPolygons(data = sf_np_union %>%
 #                                            filter(name == "阿寒摩周"))
 # なぜかもう一度... WKTが維持されないので、呼び出しごにsf::st_sf()を実行する必要がある
-np <- sf_np_union %>%
-  as_tibble() %>%
-  st_sf()
-devtools::use_data(np, overwrite = TRUE, compress = "xz")
+np <-
+  sf_np_union %>%
+  select(name, geometry) %>%
+  tibble::new_tibble(subclass = "sf")
+expect_equal(dim(np), c(34, 2))
+usethis::use_data(np, overwrite = TRUE, compress = "xz")
--- data-raw/np_information.R
+++ data-raw/np_information.R
@@ -4,38 +4,35 @@
 library(rvest)
 library(tidyverse) # dplyr, readr, stringr
 library(testthat)
-if (dir.exists("data-raw") != TRUE) {
-  devtools::use_data_raw()
+if (dir.exists(here::here("data-raw")) != TRUE) {
+  usethis::use_data_raw()
 }
 
 base_url <- "http://www.env.go.jp/nature/np/"
 
-x <- read_html("http://www.env.go.jp/park/parks/index.html") %>%
+x <-
+  read_html("http://www.env.go.jp/park/parks/index.html") %>%
   html_nodes(css = 'body > div.l-wrapper > div.l-main > div > div > div.p-map.p-spot-map.u-center.u-mb50 > div.inner > a')
 
-df_np_list <- data_frame(
-  name_en = x %>%
-    html_attr("href") %>%
-    str_replace("../", "") %>%
-    str_replace("/index.html", ""),
-  name = x %>%
-    html_text() %>%
-    str_replace("[0-9]{0,2}", "") %>%
-    str_trim()
-) %>%
-  bind_rows(
-    data_frame(
-      name_en = "amamigunto",
-      name = "奄美群島"
-    )
-  ) %>%
-  mutate(name = recode(name, `霧島錦江` = "霧島錦江湾"))
+df_np_list <-
+  tibble(
+    name_en = x %>%
+      html_attr("href") %>%
+      str_replace("../", "") %>%
+      str_replace("/index.html", ""),
+    name = x %>%
+      html_text() %>%
+      str_replace("[0-9]{0,2}", "") %>%
+      str_trim()) %>%
+  # nps_all.shp と合わせるために修正
+  mutate(name = recode(name,
+                       `霧島錦江` = "霧島錦江湾"))
 expect_equal(nrow(df_np_list), 34L)
 
 # 適宜必要な情報を追加していく
 # まだdata-rawで管理。最終的にはdataでパッケージのデータとして利用可能にする
 df_np_list %>%
-  write_csv("data-raw/np_list.csv")
+  write_csv(here::here("data-raw/np_list.csv"))
 
 # 陸域と海域
 # 慶良間, 奄美群島, 西表石垣, やんばる, 屋久島, 霧島錦江湾, 雲仙天草,
@@ -44,24 +41,25 @@ df_np_list %>%
 # ... 18
 
 ####################################
-# 指定年月日
+# [WIP] 指定年月日
 ####################################
 if (file.exists("data-raw/国立_国定公園_指定年月日一覧.pdf") == FALSE) {
   download.file("https://www.env.go.jp/nature/ari_kata/shiryou/031208-3-3.pdf",
                 "data-raw/国立_国定公園_指定年月日一覧.pdf")
 }
 
-library(tabulizer)
-library(tidyverse)
-df_list <- tabulizer::extract_tables("data-raw/国立_国定公園_指定年月日一覧.pdf",
+df_list <-
+  tabulizer::extract_tables(here::here("data-raw/国立_国定公園_指定年月日一覧.pdf"),
                                      pages = 1,
-                                     method = "matrix") %>%
+                            # listをdafa.frameにするのでmatrix
+                                     output = "matrix") %>%
   as.data.frame() %>%
   mutate_all(na_if, y = "") %>%
   mutate_all(as.character)
 
 names(df_list) <- df_list[1, ] %>% c()
-df_list %<>%
+df_list <-
+  df_list %>%
   slice(-1L) %>%
   select(-3) %>%
   rlang::set_names(c("date", "name")) %>%
