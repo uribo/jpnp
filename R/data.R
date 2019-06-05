40d94fe4e4b9037f553ce3cd11745c20e184b113
Shinya Uryu <uryu.shinya@nies.go.jp>
2019-06-05 11:57:48 +0900

    Data description
    Add copyright

---
+++ R/data.R
@@ -0,0 +1,29 @@
+#' @title Japanese National Park Area Dataset
+#'
+#' @description Japan National Park Spatial Data.
+#' @docType data
+#' @return \item{np}{a sf. The espg (SRID) is 4326. Combine into single polygon for each park and protection zone area category, namely MULTIPOLYGON.}
+#' @format A `sf` object contains 183 rows 2 variables:
+#' \itemize{
+#'   \item{name}
+#'   \item{name_en}
+#'   \item{area}
+#'   \item{geometry}
+#' }
+#' @source Natural Environmental Information GIS (Biodiversity Center of Japan, Ministry of the Environment.)
+#'
+#' \url{http://gis.biodic.go.jp/webgis/sc-026.html?kind=nps}
+#'
+#' This data is modified from GIS data that national park area by Shinya Uryu.
+#' @details
+#' National Parks represent outstanding natural landscapes of Japan. The park lands are designated
+#' and managed by the Country (MOE) in accordance with the Natural Parks Law.
+#' As of February 2018, 34 areas have been designated as the National Parks.
+#' The data includes names and classification attributes such as Special Protection Zones,
+#' Class I Special Zones, etc. Please see \url{https://www.env.go.jp/en/nature/nps/park/} the description of each zone.
+#' @examples
+#' \dontrun{
+#' np
+#' }
+#' @keywords datasets
+NULL
--- data-raw/np_area.R
+++ data-raw/np_area.R
@@ -1,32 +1,34 @@
 ####################################
+# Update: 2019-06-05
 # 環境性生物多様性センターが配布している
-# 国立公園区域等のshapefile
+# 国立公園区域等のshapefile (http://gis.biodic.go.jp/webgis/sc-026.html?kind=nps)
 # アンケートに答えてダウンロード
+# kmlならアンケート回答不要だし1ファイルで済むがst_readで読み込めない
 ####################################
 library(sf)
 library(dplyr)
 library(testthat)
-library(ggplot2) # 2.2.1.9000
+library(ggplot2)
 
 sf_np <-
-  st_read(here::here("data-raw/nps_all/nps_all.shp"), as_tibble = TRUE) %>%
+  st_read(here::here("data-raw/nps/nps_all.shp"), as_tibble = TRUE) %>%
   st_transform(crs = 4326) %>%
-  rlang::set_names(c("name", "area", "geometry"))
+  purrr::set_names(c("name", "area", "geometry"))
 expect_equal(dim(sf_np), c(8789, 3))
-expect_equal(sf_np$name %>% n_distinct(), 35L)
+expect_equal(sf_np$name %>% n_distinct(), 35L) # あとで34に修正
+expect_equal(sf_np$area %>% n_distinct(), 7L)
 
 # Modified ----------------------------------------------------------------
 # nameから34公園で区分できるように「編集」
-# 編集なのでデータの出典明記を変更する必要あり
 # source(here::here("data-raw/np_information.R"))
 df_np <-
-  readr::read_csv(here::here("data-raw/np_list.csv"))
+  readr::read_csv(here::here("data-raw/np_list.csv")) %>%
+  tibble::rowid_to_column()
 
 expect_length(
   unique(sf_np$name)[!unique(sf_np$name) %in% df_np$name],
   2L
 )
-
 sf_np_modified <-
   sf_np %>%
   mutate(name = recode(name,
@@ -38,29 +40,19 @@ expect_length(
   unique(sf_np_modified$name)[!unique(sf_np_modified$name) %in% df_np$name],
   0
 )
-# 公園ごとに一つのポリゴン (保護区でわけない)
-sf_np_union <-
+# 公園、保護区の区分ごとに一つのポリゴンにマージ
+np <-
   sf_np_modified %>%
-  split(.$name) %>%
-  purrr::map(~ st_buffer(.x, dist = 0.0001) %>%
-               st_union() %>%
-               st_simplify(preserveTopology = TRUE, dTolerance = 0.0005)) %>%
-  purrr::reduce(c) %>%
-  as_tibble() %>%
-  st_sf()
-sf_np_union$name <-
-  sort(unique(sf_np_modified$name))
-
-# mapの過程でやるとサイズが合わなくなる...
-names(sf_np_union) <- c("geometry", "name")
+  group_by(name, area) %>%
+  summarise() %>%
+  ungroup() %>%
+  left_join(df_np, by = "name") %>%
+  arrange(rowid, area) %>%
+  mutate(name_en = stringr::str_to_title(name_en)) %>%
+  select(name, name_en, area)
 
 # library(leaflet)
-# leaflet() %>% addTiles() %>% addPolygons(data = sf_np_union %>%
+# leaflet() %>% addTiles() %>% addPolygons(data = np %>%
 #                                            filter(name == "阿寒摩周"))
-# なぜかもう一度... WKTが維持されないので、呼び出しごにsf::st_sf()を実行する必要がある
-np <-
-  sf_np_union %>%
-  select(name, geometry) %>%
-  tibble::new_tibble(subclass = "sf")
-expect_equal(dim(np), c(34, 2))
+expect_equal(dim(np), c(183, 4))
 usethis::use_data(np, overwrite = TRUE, compress = "xz")
