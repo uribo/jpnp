7334fca5ed72b367f00c11a38e2cc43cfa9208b3
Shinya Uryu <uryu.shinya@nies.go.jp>
2019-11-26 16:38:50 +0900

    Fix mesh size type

--- R/np_area.R
+++ R/np_area.R
@@ -1,8 +1,7 @@
 #' Inside of National Park Meshes
 #'
-#' @param d data (national park polygon)
-#' @param mesh_size Which mesh size to output. Supports 10km and 1km.
-#'
+#' @param df data frame (national park polygon)
+#' @param mesh_size Which mesh size to output. Supports 10 and 1km.
 #' @importFrom dplyr filter select pull
 #' @importFrom jpmesh export_meshes fine_separate
 #' @importFrom purrr map pmap set_names flatten_chr
@@ -11,22 +10,19 @@
 #' @examples
 #' \dontrun{
 #' data(np, package = "np")
-#' np[11, ] %>% np_area_meshed(mesh_size = "10km")
+#' np[11, ] %>% np_area_meshed(mesh_size = 10)
 #' }
 #' @aliases np_area_meshed
 #' @export
-np_area_meshed <- function(d, mesh_size = c("10km", "1km")) {
-
+np_area_meshed <- function(df, mesh_size = c(1, 10)) {
   id <- meshcode <- res_contains <- NULL
-
   df_tmp <-
     tibble::tibble(res_contains = suppressMessages(sf::st_intersects(jpmesh::sf_jpmesh,
-                                                                             d,
+                                                                     df,
                                                                              sparse = FALSE,
                                                                              prepared = TRUE)) %>%
                              as.numeric())
-  df_tmp$id <- 1:nrow(df_tmp)
-
+  df_tmp$id <- seq_len(nrow(df_tmp))
   meshes <-
     jpmesh::sf_jpmesh[df_tmp %>%
                         dplyr::filter(res_contains == 1) %>%
@@ -35,31 +31,24 @@ np_area_meshed <- function(d, mesh_size = c("10km", "1km")) {
     purrr::map(jpmesh::fine_separate) %>%
     purrr::flatten_chr() %>%
     unique()
-
-  if (mesh_size == "1km") {
+  if (mesh_size == 1) {
     # 10km -> 1km mesh
     meshes <-
       meshes %>%
       purrr::map(jpmesh::fine_separate) %>%
       purrr::flatten_chr()
   }
-
   sf_prefmesh <-
     meshes %>%
     jpmesh::export_meshes()
-
   df_tmp <-
     tibble::tibble(res_contains = suppressMessages(sf::st_intersects(sf_prefmesh,
-                                                                     d)) %>%
+                                                                     df)) %>%
                              as.numeric())
-
-  df_tmp$id <- 1:nrow(df_tmp)
-  res <-
-    sf_prefmesh[df_tmp %>%
+  df_tmp$id <- seq_len(nrow(df_tmp))
+  sf_prefmesh[df_tmp %>%
                   dplyr::filter(!is.na(res_contains)) %>%
                   dplyr::pull(id) %>%
                        unique(), ] %>%
     dplyr::select(meshcode)
-
-  return(res)
 }
