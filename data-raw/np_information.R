21d203e12f47049f2ba14dba1ef6940b167f0838
Shinya Uryu <uryu.shinya@nies.go.jp>
2019-12-11 15:44:51 +0900

    add name_en_short column

--- data-raw/np_information.R
+++ data-raw/np_information.R
@@ -4,9 +4,8 @@
 library(rvest)
 library(tidyverse) # dplyr, readr, stringr
 library(assertr)
-if (dir.exists("data-raw") != TRUE) {
+if (dir.exists("data-raw") != TRUE)
   usethis::use_data_raw()
-}
 if (file.exists("data-raw/np_list.csv") == FALSE) {
   x <-
     read_html("http://www.env.go.jp/park/parks/index.html")
@@ -40,8 +39,8 @@ if (file.exists("data-raw/np_list.csv") == FALSE) {
         url = x %>%
           html_nodes(css = glue::glue("#cont-accordion-{.x} > ul > li > a")) %>%
           html_attr("href") %>%
-          xml2::url_absolute(base = "http://www.env.go.jp/park/parks") %>%
-          stringr::str_replace("http://www.env.go.jp/", "http://www.env.go.jp/park/")
+          xml2::url_absolute(base = "https://www.env.go.jp/park/parks") %>%
+          stringr::str_replace("https://www.env.go.jp/", "https://www.env.go.jp/park/")
       )) %>%
     verify(dim(.) == c(36, 7)) %>%
     mutate(area = recode(name,
@@ -51,20 +50,29 @@ if (file.exists("data-raw/np_list.csv") == FALSE) {
     verify(dim(.) == c(34, 7)) %>%
     arrange(id) %>%
     mutate(name = stringr::str_remove(name, "国立公園"),
-           name_en = stringr::str_remove(url,
-                                "http://www.env.go.jp/park/") %>%
+           name_en = purrr::pmap_chr(.,
+                                     ~ ..7 %>%
+                                       stringr::str_replace("/park", "/en/nature/nps/park") %>%
+                                       read_html() %>%
+                                       html_nodes(css = "h1") %>%
+                                       html_text(trim = TRUE) %>%
+                                       stringr::str_remove("National Park") %>%
+                                       stringr::str_trim()),
+           name_en_short = stringr::str_remove(url,
+                                "https://www.env.go.jp/park/") %>%
              stringr::str_remove("/index.html") %>%
              stringr::str_to_title())
   np_information <-
     np_information %>%
-    select(id, name, name_en, region, registered, area, prefecture, url) %>%
+    select(id, name, name_en, name_en_short, region, registered, area, prefecture, url) %>%
     tidyr::separate_rows(prefecture, sep = "\u30fb") %>%
     group_by(name) %>%
     tidyr::nest(prefs = c(prefecture)) %>%
     ungroup() %>%
-    verify(dim(.) == c(34, 8))
+    verify(dim(.) == c(34, 9))
   np_information %>%
     tidyr::unnest(cols = prefs) %>%
     verify(nrow(.) == 82L)
+  unique(np_information$name_en_short)
   usethis::use_data(np_information, overwrite = TRUE)
 }
